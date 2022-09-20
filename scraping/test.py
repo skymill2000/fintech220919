f1ff4255458784d1f97f66d7ac0f3893ef874374
유관우 <skymill2000@gmail.com>
2022-09-20 11:22:04 +0900

    소제목 스크래핑 하기

--- scraping/test.py
+++ scraping/test.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('./chromedriver')
 # 크롬을 통해서 스크래핑을 진행 크롬 드라이버 로딩
-driver.get('https://zdnet.co.kr/view/?no=20211018152458')
+driver.get('https://www.hankookilbo.com/News/Read/A2022091910410000417')
+
+title = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div/div[1]/h2')
+print(title.text)
