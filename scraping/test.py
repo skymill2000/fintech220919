e70965d102d809a09d012fb54b987b910a21e667
유관우 <skymill2000@gmail.com>
2022-09-20 11:30:06 +0900

    소제목 내용 스크래핑

--- scraping/test.py
+++ scraping/test.py
@@ -7,3 +7,8 @@ driver.get('https://www.hankookilbo.com/News/Read/A2022091910410000417')
 
 title = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div/div[1]/h2')
 print(title.text)
+
+description = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div/div[2]/div[2]/p')
+print(description.text)
+
+# 소제목 스크래핑하기
