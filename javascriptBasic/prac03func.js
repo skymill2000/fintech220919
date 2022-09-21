037b06794f567c68b74c60c8de13a0a0d19b2f02
유관우 <skymill2000@gmail.com>
2022-09-21 11:01:01 +0900

    arrow function 실습 완료

--- javascriptBasic/prac03func.js
+++ javascriptBasic/prac03func.js
@@ -8,6 +8,21 @@ const plus2 = (p1, p2) => {
   return p1 + p2;
 };
 
-console.log(plus2(1, 3));
-
 // #work4 arrow function 으로 minus(), div(), multi() 빼기 나누기 곱하기 기능 구현하기;
+const minus = (p1, p2) => {
+  return p1 - p2;
+};
+
+const multi = (p1, p2) => {
+  return p1 * p2;
+};
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
+console.log(plus(2, 5));
+console.log(plus2(2, 5));
+console.log(multi(1, 2));
+console.log(minus(1, 2));
+console.log(div(1, 2));
