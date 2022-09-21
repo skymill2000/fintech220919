faa4cf59622d014448ed21d852f3efff51eb4d80
유관우 <skymill2000@gmail.com>
2022-09-21 15:05:52 +0900

    useState 를 통한 스테이트 훅 구현

--- insure/src/components/EventComponent.js
+++ insure/src/components/EventComponent.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EventComponent = () => {
+  const [inputValue, setInputValue] = useState("초기값");
   const handleClick = () => {
     console.log("클릭하셨습니다");
   };
 
   const handleChange = (event) => {
     const { value } = event.target;
-    console.log(value);
+    setInputValue(value);
   };
 
   return (
     <div>
-      <p>사용자 입력 값 : </p>
+      <p>사용자 입력 값 : {inputValue}</p>
       <input onChange={handleChange}></input>
       <button onClick={handleClick}>버튼</button>
     </div>
