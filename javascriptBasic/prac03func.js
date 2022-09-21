113941a04b3119d17eba12748b2c79ee01215882
유관우 <skymill2000@gmail.com>
2022-09-21 10:46:55 +0900

    #실습4  화살표 함수 구현 연습

---
+++ javascriptBasic/prac03func.js
@@ -0,0 +1,13 @@
+function plus(p1, p2) {
+  return p1 + p2;
+}
+
+console.log(plus(1, 3));
+
+const plus2 = (p1, p2) => {
+  return p1 + p2;
+};
+
+console.log(plus2(1, 3));
+
+// #work4 arrow function 으로 minus(), div(), multi() 빼기 나누기 곱하기 기능 구현하기;
