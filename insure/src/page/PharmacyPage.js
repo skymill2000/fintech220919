5cc0696c4a300f05708f946c8cfed1aae590eb4b
유관우 <skymill2000@gmail.com>
2022-09-22 14:26:21 +0900

    약국 검색 기능 완성

--- insure/src/page/PharmacyPage.js
+++ insure/src/page/PharmacyPage.js
@@ -1,19 +1,30 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "../components/Common/AppBar";
 import SearchInput from "../components/news/SearchInput";
 
 const PharmacyPage = () => {
-  const handleChange = (event) => {};
+  const [searchInput, setSearchInput] = useState("");
+  const [pharmacyList, setPharmacyList] = useState([]);
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchInput(value);
+  };
   const handleClick = () => {
     const option = {
       method: "GET",
-      url: "",
-      params: {},
+      url: "http://apis.data.go.kr/B551182/pharmacyInfoService/getParmacyBasisList",
+      params: {
+        ServiceKey:
+          "uiu3ZzNzDB04UbxOtOL1atH04WOtxB5WSKkPbaCASVHbwgcsIPwHA5Qp6xOmSe6fzCnUVifZcfTXDkgNegv4qQ==",
+        emdongNm: searchInput,
+      },
     };
 
+    // #work6 약국 검색 api 를 활용하여 특정 동의 약국 목록을 받아오기
     axios(option).then((response) => {
-      console.log(response);
+      console.log(response.data.response.body.items.item);
+      setPharmacyList(response.data.response.body.items.item);
     });
   };
 
@@ -24,6 +35,9 @@ const PharmacyPage = () => {
         handleChange={handleChange}
         handleClick={handleClick}
       ></SearchInput>
+      {pharmacyList.map((pharmacy) => {
+        return <p>{pharmacy.yadmNm}</p>;
+      })}
     </div>
   );
 };
