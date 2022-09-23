9f0b1c12b18b87d0a26cf6572d9c346aec5bdabc
유관우 <skymill2000@gmail.com>
2022-09-23 10:53:00 +0900

    caesar.js 암호화 구현

---
+++ javascriptBasic/ceasarTest.js
@@ -0,0 +1,3 @@
+let caesar = require("./ceasar.js");
+console.log(caesar.encrypt(3, "hi how are you i am fine thank you and you"));
+console.log(caesar.decrypt(3, "kl krz duh brx l dp ilqh wkdqn brx dqg brx"));
