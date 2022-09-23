5d2c4c64b2121e61ce2111a4f16632933454117c
유관우 <skymill2000@gmail.com>
2022-09-23 11:35:49 +0900

    mydatahub 암호화 알고리즘 완성

--- insure/src/lib/encrypt.js
+++ insure/src/lib/encrypt.js
@@ -1,11 +1,27 @@
 const { createCipheriv, createHmac } = require("crypto");
-const makeMyDataHubAES = () => {
+const makeMyDataHubAES = (plainText) => {
   // alog : AES_CBC_PKCS5PADDING/256
+  let algorithm = "aes-256-cbc";
   // encKey : !w#wkW3AY5Tq#hGkh#UwAkq3TKk3s#36
+  const key = "!w#wkW3AY5Tq#hGkh#UwAkq3TKk3s#36";
   // EncIV : TIEY4xGsHK5YwTqs
+  const iv = "TIEY4xGsHK5YwTqs";
+  const cipher = createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(plainText, "utf-8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
   // plain : !Kwic123테스트
   // EncData : sd3TMem6Jv/Mn2LnvNbYDEuW7j0ESVy8Xde99V6ii5M=
 };
 
-console.log(makeMyDataHubAES("!Kwic123테스트"));
+if (
+  makeMyDataHubAES("!Kwic123테스트") ===
+  "sd3TMem6Jv/Mn2LnvNbYDEuW7j0ESVy8Xde99V6ii5M="
+) {
+  console.log("일치합니다.");
+} else {
+  console.log("일치X");
+}
+
 // return 'sd3TMem6Jv/Mn2LnvNbYDEuW7j0ESVy8Xde99V6ii5M='
+//sd3TMem6Jv/Mn2LnvNbYDEuW7j0ESVy8Xde99V6ii5M=
